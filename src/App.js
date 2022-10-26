4352cb624b1235b5491a192d1819c6756189e1f0
Sanmountain <tksgk7777@naver.com>
2022-10-26 15:24:49 +0900

    blog_practice_complete

--- src/App.js
+++ src/App.js
@@ -2,121 +2,124 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { useState } from "react";
-import Signup from "./Pages/Signup";
 
 function App() {
-  let post = "강남 우동 맛집";
-  let [글제목, titlechange] = useState([
+  //글제목
+  const [title, setTitle] = useState([
     "남자 코트 추천",
     "강남 우동 맛집",
     "파이썬 독학",
   ]);
-  let [따봉, bestchange] = useState([0, 0, 0]);
-
-  //let num = [1, 2];
-  //let [a, c] = [1, 2];
-
-  let [modal, setModal] = useState(false);
+  //발행날짜
+  const [date, setDate] = useState([
+    "2월 17일 발행",
+    "2월 28일 발행",
+    "3월 11일 발행",
+  ]);
+  //좋아요갯수
+  const [thumb, setThumb] = useState([0, 0, 0]);
+  const [modal, setModal] = useState(false);
+  const [title2, setTitle2] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   return (
-    <Router>
-      <Routes>
-        <Route path="signup" element={<Signup />} />
-      </Routes>
-      <div className="App">
-        <div className="black-nav">
-          <h4 style={{ color: "white", fontSize: "16px" }}>ReactBlog</h4>
-        </div>
-
-        <button
-          onClick={() => {
-            let copy2 = [...글제목];
-            copy2.sort();
-            titlechange(copy2);
-          }}
-        >
-          가나다순정렬
-        </button>
-
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy[0] = "여자코트 추천";
-            titlechange(copy);
-          }}
-        >
-          제목수정
-        </button>
-
-        {/* <div className="list">
-        <h4>
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              bestchange(따봉 + 1);
-            }}
-          >
-            👍
-          </span>{" "}
-          {따봉}
-        </h4>
-        <p>2월 17일 발행</p>
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{ color: "red", fontSize: "25px" }}>Sana's Blog</h4>
       </div>
 
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div> */}
+      <button
+        onClick={() => {
+          const copy = [...title];
+          copy.sort();
+          setTitle(copy);
+        }}
+      >
+        제목정렬
+      </button>
 
-        {글제목.map(function (a, i) {
-          return (
-            <div className="list">
-              <h4
+      <button
+        onClick={() => {
+          const copy = [...title];
+          copy[0] = "여자 코트 추천";
+          setTitle(copy);
+        }}
+      >
+        제목수정
+      </button>
+      {title.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+                setTitle2(i);
+              }}
+            >
+              {title[i]}{" "}
+              <span
                 onClick={() => {
-                  setModal(!modal);
+                  let copy = [...thumb];
+                  copy[i] = copy[i] + 1;
+                  setThumb(copy);
                 }}
               >
-                {글제목[i]}
-                <span
-                  onClick={() => {
-                    let copy = [...따봉];
-                    copy[i] = copy[i] + 1;
-                    bestchange(copy);
-                  }}
-                >
-                  👍
-                </span>
-                {따봉[i]}
-              </h4>
-              <p>2월 17일 발행</p>
-            </div>
-          );
-        })}
-        {modal == true ? (
-          <Modal titlechange={titlechange} 글제목={글제목} />
-        ) : null}
-      </div>
-    </Router>
+                👍
+              </span>
+              {thumb[i]}
+            </h4>
+            <p>{date[i]}</p>
+            <button
+              onClick={() => {
+                const copy = [...title];
+                copy.splice(i, 1);
+                setTitle(copy);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      })}
+
+      <input
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          console.log(inputValue);
+        }}
+      />
+      <button
+        onClick={() => {
+          const copy = [...title];
+          copy.unshift(inputValue);
+          setTitle(copy);
+        }}
+      >
+        입력
+      </button>
+
+      {modal === true ? (
+        <Modal title2={title2} setTitle={setTitle} title={title} />
+      ) : null}
+    </div>
   );
 }
 
 function Modal(props) {
   return (
-    <div className="modal">
-      <h4>{props.글제목[0]}</h4>
+    <div className="modal" style={{ background: "skyblue" }}>
+      <h4>{props.title[props.title2]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button>글수정</button>
+      <button
+        onClick={() => {
+          const copy = [...props.title];
+          copy[0] = "여자 코트 추천";
+          props.setTitle(copy);
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
 }
